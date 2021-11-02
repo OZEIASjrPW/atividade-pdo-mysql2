513ce9cded10353c03bfbe9800afe63e5edff0e8
OZEIASjrPW <ozeias.santos5@etec.sp.gov.br>
2021-11-02 00:40:18 +0000

    Insert Genero

---
+++ insert.php
@@ -0,0 +1,31 @@
+<?php
+require_once './vendor/autoload.php';
+
+use AtividadePDOMySQL\MySQLConnection;
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+   $bd = new MySQLConnection();
+
+    $comando = $bd->prepare('INSERT INTO generos (nome) VALUES (:nome)');
+    $comando->execute([':nome' => $_POST['nome']]);
+
+    header('Location:/index.php');
+}
+
+?>
+
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Novo Gênero</title>
+    </head>
+    <body>
+        <h1>Novo Gênero</h1>
+        <form action="insert.php" method="post">
+            <label for="nome">Novo do Gênero</label>
+            <input type="text" required name="nome" />
+            <button type="submit">Salvar<button>
+        </form>
+    </body>
+</html>
